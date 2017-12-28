57766facbeff5047afdf1e0eac44501ecc9b6e17
Abdo Aly <speed4392014@gmail.com>
2017-12-28 22:39:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCapitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            Console.Write(char.ToUpper(n[0]));
+            for (int i = 1; i < n.Length; i++)
+            {
+                Console.Write(n[i]);
+            }
+            
+            
+        }
+    
+    }
+}
