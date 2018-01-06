5d59fd87187fed9400633f2dbe6dca959f3c530a
Abdo Aly <speed4392014@gmail.com>
2018-01-07 01:16:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Way_Too_Long_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] words = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if(words[i].Length>10)
+                {
+                    Console.WriteLine("{0}{1}{2}",words[i][0],words[i].Length-2,words[i][words[i].Length-1]);
+                }
+                else
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+        }
+    }
+}
