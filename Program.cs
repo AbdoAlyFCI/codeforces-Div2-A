6df662739d926f4ab98576c966b0385648b3a2c8
Abdo Aly <speed4392014@gmail.com>
2018-01-07 01:18:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anton_and_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] alp = new char[26];
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = input[i]-97;
+                if (input[i] == ' ' || input[i] == '{' || input[i] == ',' || input[i] == '}')
+                {
+                    continue;
+                }
+                else
+                    if(alp[index]=='\0')
+                    {
+                        alp[index] = input[i];
+                        count++;
+                    }
+                else
+                    {
+                        continue;
+                    }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
