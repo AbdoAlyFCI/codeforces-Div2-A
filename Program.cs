12217868bf6b8ad5005eb120c67b33abef559ad2
Abdo Aly <speed4392014@gmail.com>
2018-01-07 15:36:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpful_Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            string s = Console.ReadLine();
+            if (s.Length == 1)
+            {
+                Console.WriteLine(s);
+            }
+            else
+            {
+                int n = s.Length - (s.Length / 2);
+                char[] num = new char[n];
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '+')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        num[c] = s[i];
+                        c++;
+                    }
+                }
+                Array.Sort(num);
+                for (int i = 0; i < c; i++)
+                {
+                    Console.Write(num[i]);
+                    if(i==c-1)
+                    {
+                        break;
+                    }
+                    Console.Write("+");
+                }
+            }
+        }
+    }
+}
